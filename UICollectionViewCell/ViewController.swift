ebc17f7ad626b9e8b23e4e255907eed183062b6c
Anton <xxahtoxaxx2015@gmail.com>
2023-07-01 23:45:40 +0400

    Initial Commit

---
+++ UICollectionViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionViewCell
+//
+//  Created by Антон Исаков on 01.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
