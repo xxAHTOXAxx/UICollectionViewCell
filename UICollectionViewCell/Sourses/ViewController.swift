c3f4a0c7d88bfe4042f146ff8eb49666ebe4d53b
Anton <xxahtoxaxx2015@gmail.com>
2023-07-05 00:12:28 +0400

    finish
    без картинок

--- UICollectionViewCell/Sourses/ViewController.swift
+++ UICollectionViewCell/Sourses/ViewController.swift
@@ -42,18 +42,18 @@ class CompositionalView: UIViewController {
 }
 
 extension CompositionalView: UICollectionViewDataSource, UICollectionViewDelegate {
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let item = collectionView.dequeueReusableCell(withReuseIdentifier: CompositionalCell.identifier, for: indexPath)
-//        item.backgroundColor = .systemGreen
-//        return item
-//    }
+    //    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    //        let item = collectionView.dequeueReusableCell(withReuseIdentifier: CompositionalCell.identifier, for: indexPath)
+    //        item.backgroundColor = .systemGreen
+    //        return item
+    //    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         20
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        2
+        3
     }
     
     func collectionView(_collection: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -64,7 +64,7 @@ extension CompositionalView: UICollectionViewDataSource, UICollectionViewDelegat
         case 1:
             return 10
         default:
-            return 1
+            return 6
         }
     }
     
@@ -82,7 +82,7 @@ extension CompositionalView: UICollectionViewDataSource, UICollectionViewDelegat
             return cell
         default:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CompositionalCell.identifier, for: indexPath)
-            cell.backgroundColor = .systemGreen
+            cell.backgroundColor = .systemRed
             return cell
         }
     }
@@ -101,49 +101,66 @@ extension CompositionalView: UICollectionViewDelegateFlowLayout {
     //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
     //        5
     //    }
+
     
     private func createLayout() -> UICollectionViewCompositionalLayout {
         return UICollectionViewCompositionalLayout { sectionIndex, _ in
             
             switch sectionIndex {
             case 0:
-                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-                let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
-                layoutItem.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 0)
                 
-                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1 / 2.2), heightDimension: .fractionalHeight(1 / 1.8 * 2))
-                let layoutGroup = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, repeatingSubitem: layoutItem, count: 2)
-                layoutGroup.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0)
-                
-                let layoutSection = NSCollectionLayoutSection(group: layoutGroup)
-                layoutSection.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
-                return layoutSection
-            case 1:
                 let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
                                                       heightDimension: .fractionalHeight(1))
                 let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
                 layoutItem.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 0)
+                
                 let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1 / 2.2),
-                                                       heightDimension: .fractionalWidth(1 / 1.8 * 2))
-                let layoutGroup = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, repeatingSubitem: layoutItem, count: 2)
+                                                       heightDimension: .fractionalHeight(1 / 1.8 * 2))
+                let layoutGroup = NSCollectionLayoutGroup.vertical(layoutSize: groupSize,
+                                                                   subitem: layoutItem, count: 5)
                 layoutGroup.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0)
+                
                 let layoutSection = NSCollectionLayoutSection(group: layoutGroup)
                 layoutSection.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
                 layoutSection.orthogonalScrollingBehavior = .groupPaging
+                
+                return layoutSection
+                
+            case 1:
+                
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                      heightDimension: .fractionalHeight(1))
+                let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
+                layoutItem.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0)
+                
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                       heightDimension: .absolute(44))
+                let layoutGroup = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                                   subitems: [layoutItem])
+               // layoutGroup.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0)
+                
+                let layoutSection = NSCollectionLayoutSection(group: layoutGroup)
+                layoutSection.contentInsets = NSDirectionalEdgeInsets(top: 50, leading: 10, bottom: 50, trailing: 10)
+                
                 return layoutSection
                 
             default:
                 
-                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
-                                                      heightDimension: .fractionalHeight (1))
-                let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
-                layoutItem.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 0)
-                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1 / 2.2),
-                                                       heightDimension: .fractionalWidth(1 / 1.8 * 2))
-                let layoutGroup = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, repeatingSubitem: layoutItem, count: 2)
-                layoutGroup.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0)
-                let layoutSection = NSCollectionLayoutSection(group: layoutGroup)
-                layoutSection.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+                let firstItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.6), heightDimension: .fractionalHeight (1))
+                let firstLayoutItem = NSCollectionLayoutItem (layoutSize: firstItemSize)
+                firstLayoutItem.contentInsets = NSDirectionalEdgeInsets (top: 5, leading: 5, bottom: 5, trailing: 5)
+                let secondItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight (0.5))
+                let secondLayoutItem = NSCollectionLayoutItem(layoutSize: secondItemSize)
+                secondLayoutItem.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+                let rightGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth (0.4),
+                heightDimension: .fractionalHeight (1))
+                let rightLayoutGroup = NSCollectionLayoutGroup.vertical(layoutSize: rightGroupSize, subitem: secondLayoutItem, count: 2)
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth (1), heightDimension: .estimated (500))
+                let layoutGroup = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [firstLayoutItem, rightLayoutGroup])
+                layoutGroup.contentInsets = NSDirectionalEdgeInsets (top: 0, leading: 0, bottom: 0, trailing: 20)
+                let layoutSection = NSCollectionLayoutSection (group: layoutGroup)
+                layoutSection.contentInsets = NSDirectionalEdgeInsets (top: 0, leading: 10, bottom: 0, trailing: 10)
                 layoutSection.orthogonalScrollingBehavior = .groupPaging
                 return layoutSection
             }
@@ -151,23 +168,5 @@ extension CompositionalView: UICollectionViewDelegateFlowLayout {
     }
 }
 
-            
-            
-            //
-            //            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-            //
-            //            let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
-            //            layoutItem.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
-            //
-            //            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1 / 5), heightDimension: .fractionalHeight(1 / 5 * 4))
-            //
-            //            let layoutGroup = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, repeatingSubitem: layoutItem, count: 5)
-            //            // layoutGroup.interItemSpacing = NSCollectionLayoutSpacing.fixed(5)
-            //            layoutGroup.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 2.5, bottom: 0, trailing: 2.5)
-            //
-            //            let sectionLayout = NSCollectionLayoutSection(group: layoutGroup)
-            //            sectionLayout.orthogonalScrollingBehavior = .groupPaging
-            //
-            //            return sectionLayout
-            //    }
-            //  }
+
+
